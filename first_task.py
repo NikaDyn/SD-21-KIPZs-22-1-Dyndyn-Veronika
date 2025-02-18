7f0279937e855f7d5d7cd2941618b3486dff3151
38099 <vladyslav.v.pats@ukd.edu.ua>
2025-02-18 17:46:42 +0200

    Add my line

--- first_task.py
+++ first_task.py
@@ -1,3 +1,3 @@
 print("Hi, my name is Dyndyn Veronika and I’m studying software development in college.")
 print("Hi, my name is Podletskyi Denys and I’m studying software development in college!")
-print("Hi, my name is Pats Vladyslav and I’m studying software development in college.")
+print("Hi, my name is Pats Vladyslav and I’m studying software development in college!")
