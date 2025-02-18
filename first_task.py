748152ed18de01d28647533ec5449bbba80d4864
Denys Podletsyi <denus.podletskiy@gmail.com>
2025-02-18 13:16:35 +0200

    my task

--- first_task.py
+++ first_task.py
@@ -1,2 +1,2 @@
 print("Hi, my name is Dyndyn Veronika and I’m studying software development in college.")
-print("Hi, my name is Podletskyi Denys and I’m studying software development in college.")
+print("Hi, my name is Podletskyi Denys and I’m studying software development in college!")
